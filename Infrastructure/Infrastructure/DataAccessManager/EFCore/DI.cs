ef6fbdf5f97998436d291d21ad3ba54c5cb683a1
raman-auro <radha.raman@aurosociety>
2025-09-09 17:24:39 +0530

    DI changes

--- Infrastructure/Infrastructure/DataAccessManager/EFCore/DI.cs
+++ Infrastructure/Infrastructure/DataAccessManager/EFCore/DI.cs
@@ -8,81 +8,77 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure; // ✅ Needed for MySQL
 using Serilog;
 
-namespace Infrastructure.DataAccessManager.EFCore;
-
-
-
-public static class DI
+namespace Infrastructure.DataAccessManager.EFCore
 {
-    public static IServiceCollection RegisterDataAccess(this IServiceCollection services, IConfiguration configuration)
+    public static class DI
     {
-        var connectionString = configuration.GetConnectionString("DefaultConnection");
-        var databaseProvider = configuration["DatabaseProvider"];
-
-        // Register Context
-        switch (databaseProvider)
+        public static IServiceCollection RegisterDataAccess(this IServiceCollection services, IConfiguration configuration)
         {
-            //case "MySql":
-            //    services.AddDbContext<DataContext>(options =>
-            //        options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 21)))
-            //        .LogTo(Log.Information, LogLevel.Information)
-            //        .EnableSensitiveDataLogging()
-            //    );
-            //    services.AddDbContext<CommandContext>(options =>
-            //        options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 21)))
-            //        .LogTo(Log.Information, LogLevel.Information)
-            //        .EnableSensitiveDataLogging()
-            //    );
-            //    services.AddDbContext<QueryContext>(options =>
-            //        options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 21)))
-            //        .LogTo(Log.Information, LogLevel.Information)
-            //        .EnableSensitiveDataLogging()
-            //    );
-            //    break;
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var databaseProvider = configuration["DatabaseProvider"]?.ToLower();
 
-            case "SqlServer":
-            default:
-                services.AddDbContext<DataContext>(options =>
-                    options.UseSqlServer(connectionString)
-                    .LogTo(Log.Information, LogLevel.Information)
-                    .EnableSensitiveDataLogging()
-                );
-                services.AddDbContext<CommandContext>(options =>
-                    options.UseSqlServer(connectionString)
-                    .LogTo(Log.Information, LogLevel.Information)
-                    .EnableSensitiveDataLogging()
-                );
-                services.AddDbContext<QueryContext>(options =>
-                    options.UseSqlServer(connectionString)
-                    .LogTo(Log.Information, LogLevel.Information)
-                    .EnableSensitiveDataLogging()
-                );
-                break;
+            // Register Context
+            switch (databaseProvider)
+            {
+                case "mysql":
+                    services.AddDbContext<DataContext>(options =>
+                        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+                            .LogTo(Log.Information, LogLevel.Information)
+                            .EnableSensitiveDataLogging()
+                    );
+                    services.AddDbContext<CommandContext>(options =>
+                        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+                            .LogTo(Log.Information, LogLevel.Information)
+                            .EnableSensitiveDataLogging()
+                    );
+                    services.AddDbContext<QueryContext>(options =>
+                        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+                            .LogTo(Log.Information, LogLevel.Information)
+                            .EnableSensitiveDataLogging()
+                    );
+                    break;
+
+                case "sqlserver":
+                default:
+                    services.AddDbContext<DataContext>(options =>
+                        options.UseSqlServer(connectionString)
+                            .LogTo(Log.Information, LogLevel.Information)
+                            .EnableSensitiveDataLogging()
+                    );
+                    services.AddDbContext<CommandContext>(options =>
+                        options.UseSqlServer(connectionString)
+                            .LogTo(Log.Information, LogLevel.Information)
+                            .EnableSensitiveDataLogging()
+                    );
+                    services.AddDbContext<QueryContext>(options =>
+                        options.UseSqlServer(connectionString)
+                            .LogTo(Log.Information, LogLevel.Information)
+                            .EnableSensitiveDataLogging()
+                    );
+                    break;
+            }
+
+            // Register repositories
+            services.AddScoped<ICommandContext, CommandContext>();
+            services.AddScoped<IQueryContext, QueryContext>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped(typeof(ICommandRepository<>), typeof(CommandRepository<>));
+
+            return services;
         }
 
+        public static IHost CreateDatabase(this IHost host)
+        {
+            using var scope = host.Services.CreateScope();
+            var serviceProvider = scope.ServiceProvider;
 
-        services.AddScoped<ICommandContext, CommandContext>();
-        services.AddScoped<IQueryContext, QueryContext>();
-        services.AddScoped<IUnitOfWork, UnitOfWork>();
-        services.AddScoped(typeof(ICommandRepository<>), typeof(CommandRepository<>));
+            var dataContext = serviceProvider.GetRequiredService<DataContext>();
+            dataContext.Database.EnsureCreated(); // only for dev/test
 
-
-        return services;
-    }
-
-    public static IHost CreateDatabase(this IHost host)
-    {
-        using var scope = host.Services.CreateScope();
-        var serviceProvider = scope.ServiceProvider;
-
-        // Create database using DataContext
-        var dataContext = serviceProvider.GetRequiredService<DataContext>();
-        dataContext.Database.EnsureCreated(); // Ensure database is created (development only)
-
-        return host;
+            return host;
+        }
     }
 }
-
-
